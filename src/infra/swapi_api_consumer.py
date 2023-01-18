5a68d4432cd967cd492623dabbc76efd94eee83b
odiegofarias <diego.farias.fernandes@gmail.com>
2023-01-17 22:10:09 -0300

    Feat: First interaction

---
+++ src/infra/swapi_api_consumer.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class SwapiApiConsumer:
+
+    @classmethod
+    def get_starships(self, page: int) -> any:
+        params = {'page': page}
+        response = requests.get('https://swapi.dev/api/starships/', params=params)
+
+        return response.json()
