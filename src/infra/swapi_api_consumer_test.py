cbcf5d02599e231abb33b4df799132e1ba37f4df
odiegofarias <diego.farias.fernandes@gmail.com>
2023-01-17 22:14:43 -0300

    test: Testando mock HTTP request

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,8 +1,11 @@
 from .swapi_api_consumer import SwapiApiConsumer
 
 
-def test_get_starships():
+def test_get_starships(requests_mock):
     """Testando get_starships method"""
+
+    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'qualquer': 'coisa'})
+
     swapi_api_consumer = SwapiApiConsumer()
     response = swapi_api_consumer.get_starships(page=1)
 
